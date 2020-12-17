8037b312e98d9919ea7943198026dc20f5ebaa02
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-17 00:17:52 -0300

    thunk was imported

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk' 
 import { createStore, applyMiddleware } from 'redux'
 import './index.css'
 import App from './App'
