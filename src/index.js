7bc903166fd7c2156d50adfb65e11273df2019c2
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-17 00:16:47 -0300

    applymiddleware imported

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
