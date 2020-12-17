31f36752528e9bfce55f0c27bc4d3d35fdca5103
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-17 10:22:13 -0300

    defining const thunk

--- src/thunk.js
+++ src/thunk.js
@@ -1,3 +1,21 @@
+
+const FETCH_START = 'start'
+const FETCH_SUCCESS = 'success'
+const FETCH_ERROR = 'error'
+
+const fetchStart = () => ({
+    type:  FETCH_START,
+})
+const fetchSuccess = payload => ({
+    type:  FETCH_SUCCESS,
+    payload,
+})
+const fetchError = error => ({
+    type:  FETCH_ERROR,
+    error,
+})
+
+const url = 'https://jsonplaceholder.typicode.com/users'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default payload =>
     (dispatch, getState) => {
