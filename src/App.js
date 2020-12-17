fa775ab7efae0b1edf30c69a2d2409e6f1486e50
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-17 09:39:38 -0300

    connect was set

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import logo from './logo.svg'
 import './App.css'
 
@@ -14,4 +15,4 @@ class App extends Component {
 
 
 
-export default App;
+export default connect()(App);
