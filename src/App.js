7d074c623ce3f35fbd2bbe6a954c8c8f36a4612d
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-18 19:42:46 -0300

    hors modified in accordance with the console: fetching: false, fetched: true

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
      miThunk('lala')
   } 
   render() {
+    console.log(this.props)
     return (
       <div className="App">
        
--- src/hors.js
+++ src/hors.js
@@ -1,5 +1,5 @@
 export const fetchReducer = ({ START, SUCCESS, ERROR }) => ({
     [START]: state => ({ ...state, fetching: true }),
-    [SUCCESS]: (state, { payload }) => ({ ...state, data: payload }),
-    [ERROR]: (state, { error }) => ({ ...state, error }),
+    [SUCCESS]: (state, { payload }) => ({ ...state, data: payload, fetching: false, fetched: true }),
+    [ERROR]: (state, { error }) => ({ ...state, error, fetching: false }),
   })
