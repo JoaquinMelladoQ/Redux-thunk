04c9d565c4e82c7affe1230331b3d8a269ee3f1b
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-18 21:16:04 -0300

    created const crudHOD: new DUCKS

--- src/hods.js
+++ src/hods.js
@@ -1,25 +1,27 @@
 import { makeType, asyncMac, createReducer, reduceReducers } from './ducks-helper'
 import { fetchReducer, addReducer, delReducer } from './hors'
 
-const t = makeType('thunk')
-const FETCH = t('fetch', true)
-const ADD = t('add', true)
-const DEL = t('del', true)
-const fetchAc = asyncMac(FETCH)
-const addAc = asyncMac(ADD)
-const delAc = asyncMac(DEL)
-
-
-const initialState = {
-  data: {
-    1: { name: 'Noticia' },
-  },
-  fetching: false,
-  fetched: false,
-  error: null, 
+export const crudHOD = mod => {
+    const t = makeType(mod)
+    const FETCH = t('fetch', true)
+    const ADD = t('add', true)
+    const DEL = t('del', true)
+    const fetchAc = asyncMac(FETCH)
+    const addAc = asyncMac(ADD)
+    const delAc = asyncMac(DEL)
+    
+    
+    const initialState = {
+      data: {},
+      fetching: false,
+      fetched: false,
+      error: null, 
+    }
+    const r1 = createReducer(initialState, fetchReducer(FETCH))
+    const r2 = createReducer(initialState, addReducer(ADD))
+    const r3 = createReducer(initialState, delReducer(DEL))
+    
+    return {
+        reducer: reduceReducers(r1, r2, r3),
+    }
 }
-const r1 = createReducer(initialState, fetchReducer(FETCH))
-const r2 = createReducer(initialState, addReducer(ADD))
-const r3 = createReducer(initialState, delReducer(DEL))
-
-export default reduceReducers(r1, r2, r3)
