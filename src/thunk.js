2aa046ea11e2d617b902cf93cfa799f34bceb5c6
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-18 21:09:51 -0300

    move it into hods

--- src/thunk.js
+++ src/thunk.js
@@ -1,28 +1,4 @@
-import { makeType, asyncMac, createReducer, reduceReducers } from './ducks-helper'
-import { fetchReducer, addReducer, delReducer } from './hors'
 
-const t = makeType('thunk')
-const FETCH = t('fetch', true)
-const ADD = t('add', true)
-const DEL = t('del', true)
-const fetchAc = asyncMac(FETCH)
-const addAc = asyncMac(ADD)
-const delAc = asyncMac(DEL)
-
-
-const initialState = {
-  data: {
-    1: { name: 'Noticia' },
-  },
-  fetching: false,
-  fetched: false,
-  error: null, 
-}
-const r1 = createReducer(initialState, fetchReducer(FETCH))
-const r2 = createReducer(initialState, addReducer(ADD))
-const r3 = createReducer(initialState, delReducer(DEL))
-
-export default reduceReducers(r1, r2, r3)
 // eslint-disable-next-line import/no-anonymous-default-export
 export const miThunk = paylaod => ({
   actions: fetchAc,
