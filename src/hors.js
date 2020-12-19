cc9619b56322e6d3a9c0e7a0c2d4ecc37a23a038
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-18 21:05:05 -0300

    deleting hors created

--- src/hors.js
+++ src/hors.js
@@ -8,3 +8,19 @@ export const addReducer = ({ START, SUCCESS, ERROR }) => ({
     [SUCCESS]: (state, { payload }) => ({ ...state, data: { ...state.data, ...payload }, creating: false }),
     [ERROR]: (state, { error }) => ({ ...state, error, creating: false }),
   })
+export const delReducer = ({ START, SUCCESS, ERROR }) => ({
+    [START]: state => ({ ...state, deleting: true }),
+    [SUCCESS]: (state, action) => {
+      const { payload } = action
+      const newData = Object.assign({}, state.data)
+      delete newData[payload]
+
+      return {
+        ...state,
+        data: {...newData},
+        deleting: false,
+        error: null,
+      }
+    },
+    [ERROR]: (state, { error }) => ({ ...state, error, deleting: false }),
+  })
